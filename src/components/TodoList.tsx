52b6384bb38e6dc1231d164e02db7ed43c79a306
Daniel Ungureanu <DanielC.Ungureanu@endava.com>
2021-10-14 14:16:59 +0300

    creating an app named taskify where you can add tasks

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Droppable } from "react-beautiful-dnd";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import "./style.css";
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completedTodos: Todo[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+}: Props) => {
+  return (
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todosHeading">Active Tasks</span>
+            {todos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? "dragcomplete" : ""}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todosHeading">Completed Tasks</span>
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={completedTodos}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
+export default TodoList;
